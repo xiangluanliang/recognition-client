391d0f2a34fdfea55da4bc2979be02699842f659
Yhr <23301142@bjtu.edu.cn>
2025-07-07 14:53:24 +0800

    登陆

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,7 @@
-import axios from 'axios';
+import * as userApi from './modules/user.js';
 
-// 创建一个axios实例
-const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // 指向你的Django后端API地址
-    timeout: 10000,
-});
-
-// 定义一个对象来存放所有API方法
-const api = {
-    // 对应后端的 /api/hello/
-    getHelloWorld: function() {
-        return apiClient.get('/hello/');
-    },
-
-    // 以后可以像这样添加新方法
-    // methodaaa: function(data) {
-    //     return apiClient.post('/aaa/', data);
-    // }
+const API = {
+    ...userApi,
 };
 
-export default api;
+export default API;
